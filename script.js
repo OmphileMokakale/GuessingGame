50cc957f1db51ffcb56a74e78a803e9516497aae
Okin-PC <201130339@student.uj.ac.za>
2022-02-23 14:23:45 +0200

    added the counter and styling

--- script.js
+++ script.js
@@ -8,13 +8,18 @@ let randomNumber = Math.ceil(Math.random()* 100)
 const GuessNumber = document.querySelector('.guessNumber')
 const btnGuess = document.querySelector('.btnGuess')
 const message = document.querySelector('.message')
+const counter = document.querySelector('.counter')
 
 
+var count = 0;
 
 
 function btnClicked() {
     
+
     const guessedNo = Number(GuessNumber.value);
+
+  
 
     if( guessedNo < randomNumber){
       message.innerHTML = "Your guess is too low";
@@ -23,8 +28,27 @@ function btnClicked() {
         message.innerHTML = "Your guess is too high";
       }else{
         message.innerHTML = `Correct, the secret number is ${guessedNo}`;
+        randomNumber = Math.ceil(Math.random()* 100)
+        count = -1;
+        setTimeout(function() { alert("New game starting!"); }, 100);
       }
+
+      if (count >= 10)
+        return;
+        count += 1;  
+
+      // console.log("counter:" + count);
+      counter.innerHTML = `Chances: ${count}/10`;
+
+      
+      setTimeout(function(){
+        message.innerHTML = ""
+      }, 3000);
+
+      
+  
 }
+
 
     //   function guessBtnClicked() {
     //     const guessedNo = Number(number_guess.value);
